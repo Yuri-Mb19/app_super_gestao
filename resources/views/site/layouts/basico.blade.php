9356cc1dd8a8b9b4a929f16c4b50f8be2dafd21a
Yuri Mbalane <Yurimnbalane19@gmail.com>
2025-02-01 14:39:52 -0300

    Enviando Formulario atravez do metodo Get

--- resources/views/site/layouts/basico.blade.php
+++ resources/views/site/layouts/basico.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title>Super Gestão - Sobre Nós</title>
+        <title>Super Gestao - @yield('titulo')</title>
         <link rel="stylesheet" href="{{ asset('css/estilo_basico.css') }}">
     </head>
 
     <body>
+        @include('site.layouts._partials.topo')
         @yield('conteudo')
     </body>
  </html>
