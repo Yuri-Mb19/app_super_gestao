b13c47f582615233e944c07c8737af16bf36141f
Yuri Mbalane <Yurimnbalane19@gmail.com>
2025-01-31 22:54:00 -0300

    Melhorando as Views

---
+++ resources/views/site/layouts/basico.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Super Gestão - Sobre Nós</title>
+        <link rel="stylesheet" href="{{ asset('css/estilo_basico.css') }}">
+    </head>
+
+    <body>
+        @yield('conteudo')
+    </body>
+ </html>
